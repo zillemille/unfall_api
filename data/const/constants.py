a57d389d96dbb2f606e8dc9cbe89900c0712ba17
zillemille <franco.zipfel@gmx.de>
2026-05-22 20:21:02 +0200

    file for all constants

---
+++ data/const/constants.py
@@ -0,0 +1,42 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "user": "dbw_user",
+    "password": "dbw_pass",
+    "dbname": "dbw_db"
+}
+
+BUNDESLAENDER = {
+    "01": "Schleswig-Holstein",
+    "02": "Hamburg",
+    "03": "Niedersachsen",
+    "04": "Bremen",
+    "05": "Nordrhein-Westfalen",
+    "06": "Hessen",
+    "07": "Rheinland-Pfalz",
+    "08": "Baden-Württemberg",
+    "09": "Bayern",
+    "10": "Saarland",
+    "11": "Berlin",
+    "12": "Brandenburg",
+    "13": "Mecklenburg-Vorpommern",
+    "14": "Sachsen",
+    "15": "Sachsen-Anhalt",
+    "16": "Thüringen",
+}
+
+UNFALLKATEGORIE = {
+    "1" : "Unfall mit Getöteten",
+    "2" : "Unfall mit Schwerverletzten",
+    "3" : "Unfall mit Leichtverletzten",
+}
+
+UNFALLTYP = {
+    "1": "Fahrunfall",
+    "2": "Abbiegeunfall",
+    "3": "Einbiegen / Kreuzen-Unfall",
+    "4": "Überschreiten-Unfall",
+    "5": "Unfall durch ruhenden Verkehr",
+    "6": "Unfall im Längsverkehr",
+    "7": "sonstiger Unfall",
+}
